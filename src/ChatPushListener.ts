e2d500d8dc1652ee3bc793aaf75c80b68a776bb7
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-08-02 19:04:24 +0800

    docs: update

--- src/ChatPushListener.ts
+++ src/ChatPushListener.ts
@@ -4,16 +4,30 @@
 export interface ChatPushListener {
   /**
    * Callback for when a push token is received.
+   *
+   * You will receive this notification when the token is updated.
+   * You will receive this notification when {@link ChatPushClient.getTokenAsync} is called.
+   * You may receive this notification when {@link ChatPushClient.registerPush} is called.
+   *
    * @param token The push token.
    */
   onReceivePushToken?(token: string): void;
   /**
    * Callback for when a push message is received.
+   *
+   * oppo: The notification will not be called.
+   *
    * @param message The message received.
    */
   onReceivePushMessage?(message: any): void;
   /**
    * Callback for when a push notification is clicked.
+   *
+   * fcm: The notification will not be called.
+   * huawei: The notification will not be called.
+   * honor: The notification will not be called.
+   * oppo: The notification will not be called.
+   *
    * @param message The message clicked.
    */
   onClickNotification?(message: any): void;
